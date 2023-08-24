1e14751fe53405bf59babda2f4ae8c444e2fcedf
Bernardo Padilha <bernardo_limas@hotmail.com>
2023-08-24 16:39:59 -0300

    bugfix:change text button agendamentos

--- src/components/ButtonQueries.tsx
+++ src/components/ButtonQueries.tsx
@@ -7,29 +7,28 @@ export function ButtonQueries() {
   return (
     <main className=" flex flex-col items-start justify-center fixed right-4 bottom-5 shadow-lg z-[999]">
       <div
-        className={`${
-          openButton
-            ? "scale-100 visible opacity-100"
-            : "scale-0 invisible opacity-0"
-        } origin-bottom transition-all w-full h-full backdrop-blur-sm bg-[#495E7F]/80 pt-[50px] gap-2 px-2 flex flex-col items-start justify-center rounded-tl-[50px] rounded-[7px]`}
+        className={`${openButton
+          ? "scale-100 visible opacity-100"
+          : "scale-0 invisible opacity-0"
+          } origin-bottom transition-all w-full h-full backdrop-blur-sm bg-[#495E7F]/80 pt-[50px] gap-2 px-2 flex flex-col items-start justify-center rounded-tl-[50px] rounded-[7px]`}
       >
         <div className="flex items-center justify-center gap-1">
           <div className="w-1 h-1 bg-[#CD9F97] rounded-full" />
-          <p className="text-white text-xs font-light">
+          <p className="text-white text-xs md:text-sm font-light">
             Consulta Médica Presencial
           </p>
         </div>
 
         <div className="flex items-center justify-center gap-1">
           <div className="w-1 h-1 bg-[#CD9F97] rounded-full" />
-          <p className="text-white text-xs font-light">
+          <p className="text-white text-xs md:text-sm font-light">
             Consulta Médica Online
           </p>
         </div>
 
         <div className="flex items-center justify-center gap-1">
           <div className="w-1 h-1 bg-[#CD9F97] rounded-full" />
-          <p className="text-white text-xs font-light">Visita Domiciliar</p>
+          <p className="text-white text-xs md:text-sm font-light">Visita Domiciliar</p>
         </div>
 
         <button className="w-full h-7 bg-[#465976] text-white text-xs font-light rounded-md mb-[30px]">
@@ -38,28 +37,28 @@ export function ButtonQueries() {
 
         <div className="flex items-center justify-center gap-1">
           <div className="w-1 h-1 bg-[#CD9F97] rounded-full" />
-          <p className="text-white text-xs font-light">
+          <p className="text-white text-xs md:text-sm font-light">
             Eletrocardiograma Monitoramento
           </p>
         </div>
 
         <div className="flex items-center justify-center gap-1">
           <div className="w-1 h-1 bg-[#CD9F97] rounded-full" />
-          <p className="text-white text-xs font-light">
+          <p className="text-white text-xs md:text-sm font-light">
             Monitoramento da Apneia do Sono
           </p>
         </div>
 
         <div className="flex items-center justify-center gap-1">
           <div className="w-1 h-1 bg-[#CD9F97] rounded-full" />
-          <p className="text-white text-xs font-light">
+          <p className="text-white text-xs md:text-sm md:text-sm font-light">
             Ultrassom com Doppler de Carótidas
           </p>
         </div>
 
         <div className="flex items-center justify-center gap-1">
           <div className="w-1 h-1 bg-[#CD9F97] rounded-full" />
-          <p className="text-white text-xs font-light">
+          <p className="text-white text-xs md:text-sm font-light">
             Ultrassom com Doppler Membros Inf
           </p>
         </div>
@@ -71,7 +70,7 @@ export function ButtonQueries() {
 
       <button
         onClick={() => setOpenButton(!openButton)}
-        className="w-[250px]  hover:brightness-90 transition-all h-12 flex items-center gap-2 justify-start pl-5 bg-primary-500 text-white text-xs font-light mt-[10px] rounded-lg"
+        className="w-full  hover:brightness-90 transition-all h-12 flex items-center gap-2 justify-start pl-5 bg-primary-500 text-white text-xs font-light mt-[10px] rounded-lg"
       >
         <CaretUp
           size={20}
